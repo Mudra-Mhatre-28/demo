571c081803522c0fb27c002008b56f730bd7615e
Mudra Mhatre <mudramhatre189@gmail.com>
2026-05-02 23:58:50 +0530

    adding errors

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,10 @@
 import sum from "./sum.js";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  }),
+  test("adds 7 + 9 to equal 16", () => {
+    expect(sum(7, 9)).toBe(16);
+  })
 });
